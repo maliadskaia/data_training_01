4de52d522f6507b998df9292d8ed98c2aac32538
maliadskaia <amaliadskaia@opsguru.io>
2022-06-06 18:55:29 +0300

    rename

--- AWS_databricks_&_data_governance/main.tf
+++ AWS_databricks_&_data_governance/main.tf
@@ -1,5 +1,5 @@
 # --------- s3 ---------
 resource "aws_s3_bucket" "bucket_all_inputs" {
-  bucket = "Bucket-for-all-input-files"
+  bucket = "data-training-bucket-for-all-input-files-databrix"
   tags   = merge(var.common_tags, { Name = "Bucket-for-all-input-files" })
 }
