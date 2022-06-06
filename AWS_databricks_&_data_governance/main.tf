f2fd4871db2fd610918c5735d7557e82530f0f6e
maliadskaia <amaliadskaia@opsguru.io>
2022-06-06 18:58:46 +0300

    -

--- AWS_databricks_&_data_governance/main.tf
+++ AWS_databricks_&_data_governance/main.tf
@@ -1,5 +1,5 @@
 # --------- s3 ---------
-resource "aws_s3_bucket" "bucket_all_inputs" {
+resource "aws_s3_bucket" "b" {
   bucket = "data-training-bucket-for-all-input-files-databrix"
   tags   = var.common_tags
 }
