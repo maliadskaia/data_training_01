a82c80b3cd6db17b675e6de751726de272a23267
maliadskaia <amaliadskaia@opsguru.io>
2022-06-06 19:21:33 +0300

    -

--- AWS_databricks_&_data_governance/main.tf
+++ AWS_databricks_&_data_governance/main.tf
@@ -1,6 +1,5 @@
 # --------- s3 ---------
 resource "aws_s3_bucket" "datatraining_bucket_allinputs" {
   bucket = "datatraining-allinputs"
-  acl    = "public-read-write"
   tags   = var.common_tags
 }
