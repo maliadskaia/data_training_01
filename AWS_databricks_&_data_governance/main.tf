0731a3e6f0a36962a0b8d95e5fe3f68da88c24aa
maliadskaia <amaliadskaia@opsguru.io>
2022-06-06 19:45:32 +0300

    adding 2 more buckets

--- AWS_databricks_&_data_governance/main.tf
+++ AWS_databricks_&_data_governance/main.tf
@@ -3,3 +3,13 @@ resource "aws_s3_bucket" "datatraining_bucket_allinputs" {
   bucket = "datatraining-all-inputs"
   tags   = var.common_tags
 }
+
+resource "aws_s3_bucket" "datatraining_bucket_csv" {
+  bucket = "datatraining-csv"
+  tags   = var.common_tags
+}
+
+resource "aws_s3_bucket" "datatraining_bucket_parquet" {
+  bucket = "datatraining-parquet"
+  tags   = var.common_tags
+}
