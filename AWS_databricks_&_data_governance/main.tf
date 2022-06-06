d78eabae66900f89f319fbde938c91357e0a7289
maliadskaia <amaliadskaia@opsguru.io>
2022-06-06 18:50:07 +0300

    -

--- AWS_databricks_&_data_governance/main.tf
+++ AWS_databricks_&_data_governance/main.tf
@@ -1,6 +1,5 @@
 # --------- s3 ---------
 resource "aws_s3_bucket" "bucket_all_inputs" {
   bucket = "Bucket-for-all-input-files"
-  region = var.region
   tags   = merge(var.common_tags, { Name = "Bucket-for-all-input-files" })
 }
