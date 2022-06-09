8649f9d3559d154bc596c9688ebf35f565d554a0
maliadskaia <amaliadskaia@opsguru.io>
2022-06-09 13:56:55 +0800

    adding data for lambda

--- AWS_databricks_&_data_governance/main.tf
+++ AWS_databricks_&_data_governance/main.tf
@@ -13,3 +13,8 @@ resource "aws_s3_bucket" "datatraining_bucket_parquet" {
   bucket = "datatraining-parquet"
   tags   = var.common_tags
 }
+
+# --------- lambda ---------
+data "aws_lambda_code_signing_config" "existing_csc" {
+  arn = "arn:aws:lambda:us-east-1:390148573654:function:datatraining-separate-due-to-file-format"
+}
